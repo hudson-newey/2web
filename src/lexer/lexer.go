5015df5f1dea47c18c4f7eab152ff82b056a9b32
hudson-newey <hudsonnewey@gmail.com>
2025-03-29 11:36:00 +1000

    Refactor out lexer and ast builder

---
+++ src/lexer/lexer.go
@@ -0,0 +1,51 @@
+package lexer
+
+import "strings"
+
+type lexNode struct {
+	Selector string
+	Content  string
+}
+
+func FindNodes(content string, startToken string, endToken string) []lexNode {
+	resultContent := []lexNode{}
+
+	rawContent := ""
+	inNode := false
+
+	for i := range content {
+		if i+len(startToken) > len(content) {
+			break
+		}
+
+		if content[i:i+len(startToken)] == startToken {
+			inNode = true
+			rawContent = ""
+			continue
+		}
+
+		if content[i:i+len(endToken)] == endToken {
+			inNode = false
+
+			// remove any characters that are a part of the start token
+			// that were remaining in the raw content
+			// and strip any leading/trailing whitespace
+			rawContent = rawContent[len(startToken)-1:]
+			refinedContent := strings.TrimSpace(rawContent)
+
+			contentObject := lexNode{
+				Selector: startToken + rawContent + endToken,
+				Content:  refinedContent,
+			}
+
+			resultContent = append(resultContent, contentObject)
+			continue
+		}
+
+		if inNode {
+			rawContent += string(content[i])
+		}
+	}
+
+	return resultContent
+}
--- src/ssg/ssg.go
+++ src/ssg/ssg.go
@@ -1,23 +1,19 @@
 package ssg
 
 import (
+	"hudson-newey/2web/src/lexer"
 	"hudson-newey/2web/src/ssg/modules"
 	"strings"
 )
 
-type ssgContent struct {
-	selector string
-	content  string
-}
-
 type isStable = bool
 
 func ProcessStaticSite(content string, filePath string) (string, isStable) {
-	ssgContent := extractSsgContent(content)
+	ssgContent := lexer.FindNodes(content, ssgStartToken, ssgEndToken)
 	ssgResult := content
 
-	for _, ssgItem := range ssgContent {
-		ssgCommand := strings.Split(ssgItem.content, " ")
+	for _, node := range ssgContent {
+		ssgCommand := strings.Split(node.Content, " ")
 		selectorContent := ""
 
 		if len(ssgCommand) >= 2 {
@@ -32,7 +28,7 @@ func ProcessStaticSite(content string, filePath string) (string, isStable) {
 			}
 		}
 
-		ssgResult = strings.Replace(ssgResult, ssgItem.selector, selectorContent, 1)
+		ssgResult = strings.Replace(ssgResult, node.Selector, selectorContent, 1)
 	}
 
 	// by comparing the original content with the result, we can determine if
@@ -40,47 +36,4 @@ func ProcessStaticSite(content string, filePath string) (string, isStable) {
 	unstable := strings.Contains(ssgResult, ssgStartToken) && strings.Contains(ssgResult, ssgEndToken)
 
 	return ssgResult, !unstable
-}
-
-func extractSsgContent(content string) []ssgContent {
-	resultContent := []ssgContent{}
-
-	rawContent := ""
-	inSsg := false
-
-	for i := range content {
-		if i+len(ssgStartToken) > len(content) {
-			break
-		}
-
-		if content[i:i+len(ssgStartToken)] == ssgStartToken {
-			inSsg = true
-			rawContent = ""
-			continue
-		}
-
-		if content[i:i+len(ssgEndToken)] == ssgEndToken {
-			inSsg = false
-
-			// remove any characters that are a part of the start token
-			// that were remaining in the raw content
-			// and strip any leading/trailing whitespace
-			rawContent = rawContent[len(ssgStartToken)-1:]
-			refinedContent := strings.TrimSpace(rawContent)
-
-			contentObject := ssgContent{
-				selector: ssgStartToken + rawContent + ssgEndToken,
-				content:  refinedContent,
-			}
-
-			resultContent = append(resultContent, contentObject)
-			continue
-		}
-
-		if inSsg {
-			rawContent += string(content[i])
-		}
-	}
-
-	return resultContent
 }
