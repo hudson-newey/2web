6954a53d1127a24d142b15de9d27688aa85987ab
hudson-newey <hudsonnewey@gmail.com>
2025-04-28 23:53:35 +1000

    Add static "if" condition support

--- compiler/src/compiler/controlFlow/cfModules/cfIf/if.go
+++ compiler/src/compiler/controlFlow/cfModules/cfIf/if.go
@@ -1,1 +1,24 @@
 package cfIf
+
+import "slices"
+
+// TODO: if conditions do not currently support reactive variables of other
+// javascript code like iife expressions
+func IfConditionContent(condition string, conditionalContent string) string {
+	// We follow the "C like" if statements, where there are clearly defined
+	// "falsy" values (0 in C), and any other value is counted as a "truthy"
+	// value.
+	//
+	// TODO: This has some typing issues where in an ideal world would be caught
+	// by the compiler if I ever got some time to add typing information to this
+	// compiler.
+	falsyConditions := []string{"false", "0", "undefined", "null"}
+
+	if slices.Contains(falsyConditions, condition) {
+		// return nothing, meaning that the if condition compiler selector will be
+		// replaced with an empty string (removed)
+		return ""
+	}
+
+	return conditionalContent
+}
--- compiler/src/compiler/controlFlow/controlFlow.go
+++ compiler/src/compiler/controlFlow/controlFlow.go
@@ -2,6 +2,7 @@ package controlFlow
 
 import (
 	"hudson-newey/2web/src/compiler/controlFlow/cfModules/cfFor"
+	"hudson-newey/2web/src/compiler/controlFlow/cfModules/cfIf"
 	"hudson-newey/2web/src/lexer"
 	"hudson-newey/2web/src/ssg"
 	"strings"
@@ -17,9 +18,18 @@ func ProcessControlFlow(filePath string, content string) string {
 		if len(node.Tokens) >= 2 {
 			ssgKeyword := node.Tokens[0]
 
+			// we "continue" in the default case so that unrecognized keywords are
+			// preserved so that ssg might be able to process them later on
+			//
+			// TODO: this is currently flawed because control flow inside of ssg
+			// "include" statements will not work
+			// we should therefore have a unified method to run both control flow and
+			// ssg at compile time until the template is stable.
 			switch ssgKeyword {
 			case forToken[0]:
 				selectorContent = cfFor.ForLoopContent(node.Tokens[1], node.Tokens[2])
+			case ifToken[0]:
+				selectorContent = cfIf.IfConditionContent(node.Tokens[1], node.Tokens[2])
 			default:
 				continue
 			}
