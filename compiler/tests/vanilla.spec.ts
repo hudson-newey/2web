13f5c2a9db8f1a11bcb99a4492c067926c7cfada
hudson-newey <hudsonnewey@gmail.com>
2025-05-24 23:31:05 +1000

    Add vanilla counterpart to compiler dev pages

---
+++ compiler/tests/vanilla.spec.ts
@@ -0,0 +1,8 @@
+import { test } from "vitest";
+import { assertNoErrors } from "./helpers/assertions";
+import { navigateToPage } from "./helpers/fixture";
+
+test("should load", () => {
+  const document = navigateToPage("vanilla.html");
+  assertNoErrors(document);
+});
