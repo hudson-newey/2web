18df55249bceb35053442d050068f72641d2481f
hudson-newey <hudsonnewey@gmail.com>
2025-07-20 22:30:49 +1000

    Do not use artifact cache in prod builds

--- compiler/src/builder/writer.go
+++ compiler/src/builder/writer.go
@@ -6,14 +6,26 @@ import (
 )
 
 func compileAndWriteFile(inputPath string, outputPath string) {
-	cacheDisabled := *cli.GetArgs().DisableCache
+	args := cli.GetArgs()
+	cacheDisabled := *args.DisableCache
+	production := *args.IsProd
 
 	// We keep a record of the last modified time of all recent input files, so
 	// that we can skip re-compiling source files that have not changed.
 	//
 	// We have to pass in the output path so that we can validate that the output
 	// file wasn't deleted by the user or an external process.
-	if !cacheDisabled {
+	//
+	// Additionally, production build speeds should be concerned with
+	// correctness instead of build speed.
+	// We therefore, skip the build cache if we are building for production, so
+	// that cache errors are (almost) impossible in production builds.
+	//
+	// Additionally, production builds have some additional processing that dev
+	// builds do not.
+	// Therefore, our method of checking modified times does not work if the
+	// build output is different.
+	if !cacheDisabled && !production {
 		hasCached := cache.IsCached(inputPath, outputPath)
 		if hasCached {
 			cli.PrintBuildLog("\t- " + inputPath + " \033[36m(cached)\033[0m")
