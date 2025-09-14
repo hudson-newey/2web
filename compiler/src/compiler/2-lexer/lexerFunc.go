2ec711735f40da12d80f30b58e9ffe34f0174516
hudson-newey <hudsonnewey@gmail.com>
2025-09-14 17:26:40 +1000

    Flat lexFunc

--- compiler/src/compiler/2-lexer/lexerFunc.go
+++ compiler/src/compiler/2-lexer/lexerFunc.go
@@ -10,81 +10,79 @@ type LexFunc func(*Lexer) (V2LexNode, LexFunc)
 
 func lexerFactory(lexMap lexDefMap, state states.LexState) LexFunc {
 	return func(lexerModel *Lexer) (V2LexNode, LexFunc) {
-		for {
-			matchingLexNode, nextState := lexMap.matching(lexerModel, state)
-			if nextState != nil {
-				return matchingLexNode, nextState
-			}
+		matchingLexNode, nextState := lexMap.matching(lexerModel, state)
+		if nextState != nil {
+			return matchingLexNode, nextState
+		}
 
-			// Some systems use \r\n for new lines, but because we ignore \r, it will
-			// work fine.
-			readerChar, _, err := lexerModel.nextChar()
-			if readerChar == '\n' {
-				lexerModel.lineFeed()
-				return V2LexNode{
-					Pos: Position{
-						Row: lexerModel.Pos.Row - 1,
-						Col: lexerModel.Pos.Col - 1,
-					},
-					Token:   lexerTokens.NewLine,
-					State:   state,
-					Content: "\n",
-				}, lexerModel.State
-			}
+		// Some systems use \r\n for new lines, but because we ignore \r, it will
+		// work fine.
+		readerChar, _, err := lexerModel.nextChar()
+		if readerChar == '\n' {
+			lexerModel.lineFeed()
+			return V2LexNode{
+				Pos: Position{
+					Row: lexerModel.Pos.Row - 1,
+					Col: lexerModel.Pos.Col - 1,
+				},
+				Token:   lexerTokens.NewLine,
+				State:   state,
+				Content: "\n",
+			}, lexerModel.State
+		}
 
-			if err != nil {
-				if err == io.EOF {
-					position := Position{
-						Row: lexerModel.Pos.Row,
-						Col: lexerModel.Pos.Col,
-					}
-
-					// We still return a textLexer state here so that if a file is partially
-					// corrupted and contains an EOF before the real end, we can recover
-					// gracefully.
-					lexNode := V2LexNode{
-						Pos:     position,
-						Token:   lexerTokens.EOF,
-						Content: "",
-						State:   states.SourceText,
-					}
-
-					return lexNode, lexerModel.State
+		if err != nil {
+			if err == io.EOF {
+				position := Position{
+					Row: lexerModel.Pos.Row,
+					Col: lexerModel.Pos.Col,
 				}
 
-				panic(err)
+				// We still return a textLexer state here so that if a file is partially
+				// corrupted and contains an EOF before the real end, we can recover
+				// gracefully.
+				lexNode := V2LexNode{
+					Pos:     position,
+					Token:   lexerTokens.EOF,
+					Content: "",
+					State:   states.SourceText,
+				}
+
+				return lexNode, lexerModel.State
 			}
 
-			startPos := lexerModel.Pos
-			lexerModel.backup(1)
-			text := lexerModel.lexLiteral(lexMap)
-
-			// There are different types of text depending on what context we are in
-			// Sometimes it can be external source code.
-			tokenMap := map[states.LexState]lexerTokens.LexToken{
-				states.ScriptSource: lexerTokens.ScriptSource,
-				states.StyleSource:  lexerTokens.ScriptSource,
-				states.TextContent:  lexerTokens.TextContent,
-			}
-
-			token, exists := tokenMap[state]
-			if !exists {
-				token = lexerTokens.TextContent
-			}
-
-			position := Position{
-				Row: startPos.Row,
-				Col: startPos.Col,
-			}
-
-			lexNode := V2LexNode{
-				Pos:     position,
-				Token:   token,
-				State:   state,
-				Content: string(text),
-			}
-
-			return lexNode, lexerModel.State
+			panic(err)
 		}
+
+		startPos := lexerModel.Pos
+		lexerModel.backup(1)
+		text := lexerModel.lexLiteral(lexMap)
+
+		// There are different types of text depending on what context we are in
+		// Sometimes it can be external source code.
+		tokenMap := map[states.LexState]lexerTokens.LexToken{
+			states.ScriptSource: lexerTokens.ScriptSource,
+			states.StyleSource:  lexerTokens.ScriptSource,
+			states.TextContent:  lexerTokens.TextContent,
+		}
+
+		token, exists := tokenMap[state]
+		if !exists {
+			token = lexerTokens.TextContent
+		}
+
+		position := Position{
+			Row: startPos.Row,
+			Col: startPos.Col,
+		}
+
+		lexNode := V2LexNode{
+			Pos:     position,
+			Token:   token,
+			State:   state,
+			Content: string(text),
+		}
+
+		return lexNode, lexerModel.State
 	}
 }
