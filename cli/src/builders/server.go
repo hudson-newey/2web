f31a654bdaebb8748fcd8d87ebac1310f00c02a1
hudson-newey <hudsonnewey@gmail.com>
2025-10-14 21:19:13 +1000

    Check pathTarget exists before starting dev server

--- cli/src/builders/server.go
+++ cli/src/builders/server.go
@@ -1,7 +1,11 @@
 package builders
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/hudson-newey/2web-cli/src/builders/configs"
+	"github.com/hudson-newey/2web-cli/src/cli"
 	"github.com/hudson-newey/2web-cli/src/packages"
 	"github.com/hudson-newey/2web-cli/src/shell"
 )
@@ -17,6 +21,13 @@ func ServeSolution(args []string) {
 func serveBrowser(args []string) {
 	viteConfig, err := configs.ViteConfigLocation()
 	pathTarget := entryTarget(args)
+
+	// Check that the path target actually exists.
+	// If it does not, we want to log a warning.
+	if _, err := os.Stat(pathTarget); os.IsNotExist(err) {
+		warningMsg := fmt.Sprintf("the specified path target does not exist: '%s'", pathTarget)
+		cli.PrintWarning(warningMsg)
+	}
 
 	if err == nil {
 		packages.ExecutePackage("vite", pathTarget, "--config", viteConfig)
