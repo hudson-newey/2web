0ce5c71b4adbc7bad4813e5b1177f023f0e023c3
hudson-newey <hudsonnewey@gmail.com>
2025-10-31 20:27:53 +1000

    Use uglifyjs for production minification

--- compiler/src/optimizer/minify/javascript.go
+++ compiler/src/optimizer/minify/javascript.go
@@ -1,13 +1,29 @@
 package minify
 
 import (
+	"fmt"
+	"os/exec"
+	"regexp"
+
 	"github.com/tdewolff/minify/v2"
 	"github.com/tdewolff/minify/v2/js"
 )
 
 func MinifyJs(content string) string {
 	m := minify.New()
-	m.AddFunc("application/javascript", js.Minify)
+
+	// Check if the "uglifyjs" command-line tool is available
+	// and use it for minification if so.
+	// Fallback to the built-in minifier otherwise.
+	if _, err := exec.LookPath("uglifyjs"); err == nil {
+		m.AddCmdRegexp(
+			regexp.MustCompile("^(application|text)/(x-)?(java|ecma)script$"),
+			exec.Command("uglifyjs"),
+		)
+	} else {
+		fmt.Println("Warning: 'uglifyjs' not found in PATH. Using the built-in minifier may cause larger file sizes.")
+		m.AddFunc("application/javascript", js.Minify)
+	}
 
 	minifiedContent, err := m.String("application/javascript", content)
 	if err != nil {
