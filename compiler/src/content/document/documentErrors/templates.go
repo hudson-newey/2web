12cdecea0f991dd05c5cea3c0a9c9de8f8a8efd2
hudson-newey <hudsonnewey@gmail.com>
2025-11-09 03:07:50 +1000

    Statically compute error template at build

--- compiler/src/content/document/documentErrors/templates.go
+++ compiler/src/content/document/documentErrors/templates.go
@@ -11,20 +11,18 @@ var errorTemplate string
 //go:embed static/errorTemplate.css
 var errorTemplateCSS string
 
+// To include the CSS in the HTML template, I hacked together a solution
+// where I replace a substring placeholder with the actual CSS content.
+//
+// TODO: Fix this properly in the future with a correct templating approach.
+var replacementTarget string = "/* __2web:compiler-internal error-template-styles */"
+var finalTemplate string = strings.Replace(
+	errorTemplate,
+	replacementTarget,
+	errorTemplateCSS,
+	1,
+)
+
 func errorHtmlSource() string {
-	errorTemplate := errorTemplate
-
-	// To include the CSS in the HTML template, I hacked together a solution
-	// where I replace a substring placeholder with the actual CSS content.
-	//
-	// TODO: Fix this properly in the future with a correct templating approach.
-	replacementTarget := "/* __2web:compiler-internal error-template-styles */"
-	errorTemplate = strings.Replace(
-		errorTemplate,
-		replacementTarget,
-		errorTemplateCSS,
-		1,
-	)
-
-	return errorTemplate
+	return finalTemplate
 }
