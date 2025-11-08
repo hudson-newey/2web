92c2c291dc04f42a4259d216da036137e60a12c7
hudson-newey <hudsonnewey@gmail.com>
2025-11-09 03:05:39 +1000

    Separate error template css into seperate file

--- compiler/src/content/document/documentErrors/templates.go
+++ compiler/src/content/document/documentErrors/templates.go
@@ -2,11 +2,29 @@ package documentErrors
 
 import (
 	_ "embed"
+	"strings"
 )
 
 //go:embed static/errorTemplate.html
 var errorTemplate string
 
+//go:embed static/errorTemplate.css
+var errorTemplateCSS string
+
 func errorHtmlSource() string {
+	errorTemplate := errorTemplate
+
+	// To include the CSS in the HTML template, I hacked together a solution
+	// where I replace a substring placeholder with the actual CSS content.
+	//
+	// TODO: Fix this properly in the future with a correct templating approach.
+	replacementTarget := "/* __2web:compiler-internal error-template-styles */"
+	errorTemplate = strings.Replace(
+		errorTemplate,
+		replacementTarget,
+		errorTemplateCSS,
+		1,
+	)
+
 	return errorTemplate
 }
