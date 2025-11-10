d58b729d743a65e9ca66b248b065107e16121127
hudson-newey <hudsonnewey@gmail.com>
2025-11-10 21:38:42 +1000

    Add tryCatch to iron package

--- packages/iron/src/errors/tryCatch.ts
+++ packages/iron/src/errors/tryCatch.ts
@@ -1,1 +1,15 @@
-export function tryCatch() {}
+/**
+ * @description
+ * Requires you to handle error cases.
+ */
+export function tryCatch<T>(fn: () => T): T | Error {
+  try {
+    return fn();
+  } catch (error) {
+    if (error instanceof Error) {
+      return error;
+    }
+
+    return new Error(String(error));
+  }
+}
