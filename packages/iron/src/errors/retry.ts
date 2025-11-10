7038500fd63584545b84dece1468943ed61f0672
hudson-newey <hudsonnewey@gmail.com>
2025-11-10 21:28:11 +1000

    Add retry module to iron package

--- packages/iron/src/errors/retry.ts
+++ packages/iron/src/errors/retry.ts
@@ -1,1 +1,25 @@
-export async function retry() {}
+export interface RetryOptions {
+  interval?: number;
+  times?: number;
+}
+
+export async function retry<ReturnType>(
+  fn: () => ReturnType | Promise<ReturnType>,
+  options: RetryOptions,
+) {
+  const { interval = 1000, times = 3 } = options;
+
+  let attempt = 0;
+  while (attempt < times) {
+    try {
+      return await fn();
+    }
+    catch (error) {
+      attempt++;
+      if (attempt >= times) {
+        throw error;
+      }
+      await new Promise((resolve) => setTimeout(resolve, interval));
+    }
+  }
+}
