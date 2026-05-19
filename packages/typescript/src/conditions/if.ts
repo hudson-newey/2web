ffaae68a32e1d3c09aa423c0e92667b7aff3f888
hudson-newey <hudsonnewey@gmail.com>
2026-05-19 20:13:20 +1000

    Update jsdoc for if condition

--- packages/typescript/src/conditions/if.ts
+++ packages/typescript/src/conditions/if.ts
@@ -2,14 +2,14 @@ import type { CompileError } from "./error";
 
 /**
  * @description
- * A conditional type tha can be used to void a type intersection based on a
+ * A conditional type that can be used to void a type intersection based on a
  * boolean condition.
  *
  * @example
  * ```ts
- * type WithToString<T> = T & If<T extends { toString(): string }>;
+ * type WithToString<T> = T & If<T, { toString(): string }>;
  * ```
  */
-export type If<Type, ExpectedType> = Type extends ExpectedType
+export type If<Type, Expected> = Type extends Expected
   ? unknown
   : CompileError<`Type condition failed`>;
