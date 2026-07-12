72743a10294b56982617ea5e1d636fc0083c64ae
hudson-newey <hudsonnewey@gmail.com>
2026-07-12 13:49:24 +1000

    EscapeHtml string replacer

--- compiler/src/content/html/escape.go
+++ compiler/src/content/html/escape.go
@@ -3,18 +3,13 @@ package html
 import "strings"
 
 func EscapeHtml(htmlFragment string) string {
-	replacementTable := map[string]string{
-		"&":  "&amp;",
-		"\"": "&quot;",
-		"'":  "&#x27;",
-		"<":  "&lt;",
-		">":  "&gt;",
-	}
+	sr := strings.NewReplacer(
+		"&", "&amp;",
+		"\"", "&quot;",
+		"'", "&#x27;",
+		"<", "&lt;",
+		">", "&gt;",
+	)
 
-	result := htmlFragment
-	for key, value := range replacementTable {
-		result = strings.ReplaceAll(result, key, value)
-	}
-
-	return result
+	return sr.Replace(htmlFragment)
 }
