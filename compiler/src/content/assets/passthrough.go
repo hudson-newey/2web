26b5f4d25556967392491f8db49c74dc320b6cf3
hudson-newey <hudsonnewey@gmail.com>
2026-09-05 21:13:33 +1000

    Fix wasm & pdf passthroguh

--- compiler/src/content/assets/passthrough.go
+++ compiler/src/content/assets/passthrough.go
@@ -1,6 +1,8 @@
 package assets
 
-import "slices"
+import (
+	"strings"
+)
 
 // Some assets should be passed through without modification.
 // Some assets like images DO NOT go through this pipeline and instead have
@@ -8,5 +10,10 @@ import "slices"
 // type conversion.
 func ShouldUseAssetPassthrough(filePath string) bool {
 	assetExtensions := []string{".pdf", ".wasm"}
-	return slices.Contains(assetExtensions, filePath)
+	for _, ext := range assetExtensions {
+		if strings.HasSuffix(filePath, ext) {
+			return true
+		}
+	}
+	return false
 }
